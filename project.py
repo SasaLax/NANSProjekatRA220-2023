24ccf050c3ec9539ce3651cb4d034cee0f1a2f46
SasaLax <sasa.lakicbn1@gmail.com>
2026-02-15 19:22:59 +0100

    Pocetak projekta. Importi.

---
+++ project.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+import statsmodels as sm
+
+from prophet import Prophet
+
+import tensorflow as tf
+
+import sklearn
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+def main():
